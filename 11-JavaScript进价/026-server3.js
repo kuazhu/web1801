c34ac0cdb18dc3c86649a3e742c6529466b10883
TomChen <76876201@qq.com>
2018-05-25 19:47:48 +0800

    get and post request for ajax

--- 11-JavaScript进价/026-server3.js
+++ 11-JavaScript进价/026-server3.js
@@ -2,21 +2,32 @@
 * @Author: TomChen
 * @Date:   2018-05-24 18:08:59
 * @Last Modified by:   TomChen
-* @Last Modified time: 2018-05-25 18:08:57
+* @Last Modified time: 2018-05-25 18:37:15
 */
 var http = require('http');
 var fs = require('fs');
+var url = require('url');
 
 var server = http.createServer(function(req,res){
 	res.setHeader("Content-Type","text/html;charset=UTF-8");
-	// res.setHeader("Content-Type","text/plain");
-	// res.statusCode = 404;
-	console.log(req.url);
-	if(req.url == '/favicon.ico'){
+	var urlStr = req.url;
+	console.log("req url:::",urlStr);
+	//如果请求的是/favicon.ico直接返回
+	if(urlStr == '/favicon.ico'){
 		res.statusCode = 404;
 		res.end();
 	}
-	var filePath = "./"+req.url;
+	//如果请求中有参数,把参数返回给前端页面
+	if(urlStr.search(/\?/) != -1){
+		var prams = url.parse(urlStr,true).query;
+		//通常拿到参数后需要根据参数做相应的处理
+		//todo......
+		var parmsStr = JSON.stringify(prams);
+		res.statusCode = 200;
+		res.end(parmsStr);		
+	}
+	//如果没有参数,打开文件读取并且返回
+	var filePath = "./"+urlStr;
 	fs.readFile(filePath,function(err,data){
 		if(err){
 			console.log('read file error:::',err);
@@ -27,8 +38,6 @@ var server = http.createServer(function(req,res){
 			res.end(data);
 		}
 	})
-
-	// res.end('<h1>hello nodejs,你好</h1>');
 });
 
 server.listen(3000,'127.0.0.1',function(){
